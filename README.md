67263d9a59508c1dbcb8b90dc47440216530f62f
Emil Bay <github@tixz.dk>
2018-02-10 17:47:48 +0100

    Fixed bugs, made abstract-encoding compliant

---
+++ README.md
@@ -0,0 +1,31 @@
+# `base32-encoding `
+
+>
+
+## Usage
+
+```js
+var  = require('base32-encoding')
+```
+
+## API
+
+### `base32.encode(buf, [output], [offset])`
+
+### `base32.decode(buf, [output], [offset])`
+
+### `base32.encodingLength(buf)`
+
+### `base32.stringify(buf, [alphabet])`
+
+### `base32.parse(buf, [alphabet])`
+
+## Install
+
+```sh
+npm install base32-encoding
+```
+
+## License
+
+[ISC](LICENSE)
--- test.js
+++ test.js
@@ -3,9 +3,17 @@ var base32 = require('.')
 var assert = require('assert')
 
 for (var i = 0; i < 1e5; i++) {
-  var a = randomBytes(Math.random() * 32 | 0)
+  var buf = randomBytes(3)
 
-  var str = base32.encode(a)
-  console.log(str)
-  assert(a.equals(base32.decode(str)))
+  var enc = base32.encode(buf)
+  var dec = base32.decode(enc)
+  assert(buf.equals(dec))
+}
+
+function bitstring (arr, i) {
+  var pad = '00000000'
+  return from(arr, function (n) {
+    var s = n.toString(2)
+    return pad.slice(0, i - s.length) + s
+  }).join('')
 }
